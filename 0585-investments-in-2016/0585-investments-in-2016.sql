f0ec51b699ca45ba95c719988502008747215a99
Aaradhya krishna raj <aaradhyakrishnaraj@gmail.com>
2026-09-06 23:32:46 +0530

    Time: 687 ms (36.04%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,13 @@
+SELECT ROUND(SUM(tiv_2016),2) as tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN(
+    select tiv_2015
+    from Insurance
+    group by tiv_2015
+    having count(*)>1
+) AND (lat,lon) IN(
+    select lat, lon
+    from Insurance
+    group by lat, lon
+    having count(*)=1
+)
